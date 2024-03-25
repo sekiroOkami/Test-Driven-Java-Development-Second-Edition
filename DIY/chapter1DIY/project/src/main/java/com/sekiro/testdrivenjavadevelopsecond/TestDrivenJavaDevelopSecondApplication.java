5bd3e5ebb47ed33187f140287b06689758de2d43
global <global@mail.com>
2024-03-25 13:36:26 +0700

    create DIY

---
+++ DIY/chapter1DIY/project/src/main/java/com/sekiro/testdrivenjavadevelopsecond/TestDrivenJavaDevelopSecondApplication.java
@@ -0,0 +1,13 @@
+package com.sekiro.testdrivenjavadevelopsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenJavaDevelopSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDrivenJavaDevelopSecondApplication.class, args);
+	}
+
+}
---
+++ DIY/test-driven-java-develop-second/src/test/java/com/sekiro/testdrivenjavadevelopsecond/TestDrivenJavaDevelopSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekiro.testdrivenjavadevelopsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenJavaDevelopSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
